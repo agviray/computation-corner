0f78abd57814310f1be6bb43b0de891df295da47
Allan Viray <allang.v.dev@gmail.com>
2024-02-11 17:39:18 -0600

    Update Navbar styles and add hover effects to links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,7 +31,7 @@ const Navbar = () => {
   const renderedNavmenuItems = NAV_ITEMS.map(({ text, href }, index) => (
     <li key={index}>
       <Link href={href} onClick={toggleMenu}>
-        <span className="inline-block w-full text-center py-3 md:px-4 md:py-0 md:leading-[64px]">
+        <span className="inline-block w-full text-white text-center py-3 md:px-4 md:py-0 md:leading-[64px] hover:tracking-[1.5px] hover:text-darkLime hover:bg-black transition transition-all">
           {text}
         </span>
       </Link>
@@ -40,15 +40,15 @@ const Navbar = () => {
 
   return (
     <header className="fixed top-0 left-0 right-0 w-full z-50">
-      <nav className="relative h-navbarHeight bg-[red]">
+      <nav className="relative h-navbarHeight bg-darkestDarkGreen">
         <div className="innerContainer relative flex justify-between items-center h-full px-4">
           <div className="logoContainer">
             <Link href="/">
-              <span>ComCor</span>
+              <span className="text-darkLime">ComCor</span>
             </Link>
           </div>
           <div
-            className={`navMenu fixed top-0 right-0 bottom-0 w-9/12 py-24 bg-[lightgray] z-[1] transition transition-all md:relative md:flex md:w-auto md:h-full md:py-0 md:translate-x-0 md:transition-none ${
+            className={`navMenu fixed top-0 right-0 bottom-0 w-9/12 py-24 bg-darkerDarkGreen z-[1] transition transition-all md:relative md:flex md:w-auto md:h-full md:py-0 md:bg-transparent md:translate-x-0 md:transition-none ${
               isMenuOpen ? 'translate-x-[0]' : 'translate-x-full'
             }`}
           >
@@ -61,24 +61,24 @@ const Navbar = () => {
             onClick={toggleMenu}
           >
             <div
-              className={`relative w-full h-[1px] bg-black z-[1] transition transition-all ${
+              className={`relative w-full h-[1px] bg-white z-[1] transition transition-all ${
                 isMenuOpen ? 'rotate-[135deg]' : 'rotate-0'
               }`}
             >
               <span
-                className={`absolute w-full h-[1px] bg-black z-[1] transition transition-all ${
+                className={`absolute w-full h-[1px] bg-white z-[1] transition transition-all ${
                   isMenuOpen ? 'top-[0]' : 'top-[-7px]'
                 }`}
               ></span>
               <span
-                className={`absolute w-full h-[1px] bg-black z-[1] transition transition-all ${
+                className={`absolute w-full h-[1px] bg-white z-[1] transition transition-all ${
                   isMenuOpen ? 'top-[0] rotate-90' : 'top-[7px] rotate-0'
                 }`}
               ></span>
             </div>
           </div>
           <div
-            className={`overlay fixed inset-0 bg-transparentWhite transition transition-all md:hidden ${
+            className={`overlay fixed inset-0 bg-transparentBlack transition transition-all md:hidden ${
               isMenuOpen ? 'visible opacity-100' : 'invisible opacity-0'
             }`}
             onClick={toggleMenu}
