658f43120efcdda056077ccfdba0bf1abc03d97a
Allan Viray <allang.v.dev@gmail.com>
2024-02-12 18:51:22 -0600

    Edit HomePage hero styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,23 @@
+import Link from 'next/link';
+
 const HomePage = () => {
   return (
-    <main>
-      <section>
-        <div className="innerContainer flex flex-col justify-center items-center min-h-screen">
-          <h1>Computation Corner</h1>
+    <main className="pt-offsetNavbarHeight">
+      <section className="bg-black">
+        <div className="hero py-14 md:py-28 pl-4">
+          <h1 className="font-notoSansMono text-darkLime font-bold text-3xl tracking-wide md:text-4xl md:tracking-wider">
+            <span className="inline-flex flex-col">
+              <span className="inline-block">COMPUTATION</span>
+              <span className="inline-block">CORNER</span>
+            </span>
+          </h1>
+          <div className="pt-6">
+            <Link href="/about">
+              <span className="inline-flex items-center h-10 px-4 text-white text-l font-notoSansMono tracking-wide border-solid border border-darkLime">
+                WHAT&apos;S THIS?
+              </span>
+            </Link>
+          </div>
         </div>
       </section>
     </main>
