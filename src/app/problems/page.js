3ee4d63930d3becec7ca4104c09efe53fd300762
Allan Viray <allang.v.dev@gmail.com>
2024-02-17 18:37:49 -0600

    Update ProblemsPage and GlossaryPage styles

--- src/app/problems/page.js
+++ src/app/problems/page.js
@@ -2,11 +2,32 @@ const ProblemsPage = () => {
   return (
     <main className="pt-offsetNavbarHeight">
       <section>
-        <div>
-          <h1>Problems Page</h1>
-          <p>This is the Problems page.</p>
+        <div className="hero py-14 md:py-28 bg-black">
+          <h1 className="font-notoSansMono font-bold text-center text-darkLime text-3xl md:text-4xl tracking-wide">
+            Problems
+          </h1>
         </div>
       </section>
+      <div className="px-5 py-20 md:py-28">
+        <section>
+          <div className="max-w-3xl mx-auto">
+            <p className="font-archivo font-light text-lightBlack text-lg tracking-wider">
+              This is the Problems Page.
+            </p>
+            <p className="pt-6 font-archivo font-light text-lightBlack text-lg tracking-wider">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, ex
+              amet tempora veniam fuga illum vitae sit eaque tempore laboriosam
+              harum atque illo repellat! Nesciunt.
+            </p>
+            <p className="pt-6 font-archivo font-light text-lightBlack text-lg tracking-wider">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+              facilis nesciunt culpa ad molestiae illo soluta totam vero animi
+              repellat, vitae cum, est veritatis consequuntur qui asperiores
+              inventore iste. Non?
+            </p>
+          </div>
+        </section>
+      </div>
     </main>
   );
 };
