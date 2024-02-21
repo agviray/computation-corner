33656287f80bfdc66009f3ee310692cc6c818810
Allan Viray <allang.v.dev@gmail.com>
2024-02-20 20:46:10 -0600

    Add hover and active styles to ThemedLink component

--- src/components/ThemedLink.js
+++ src/components/ThemedLink.js
@@ -4,7 +4,7 @@ const ThemedLink = ({ href, text }) => {
   return (
     <>
       <Link href={href}>
-        <span className="inline-flex items-center h-10 px-4 text-white text-l font-notoSansMono tracking-wide border-solid border border-darkLime lg:h-12 lg:px-6">
+        <span className="inline-flex items-center h-10 px-4 text-white text-l font-notoSansMono tracking-wide border-solid border border-darkLime lg:h-12 lg:px-6 hover:text-darkLime hover:bg-darkerDarkGreen hover:border-darkerDarkGreen active:text-white active:bg-black transition ease-in transition-all">
           {text}
         </span>
       </Link>
